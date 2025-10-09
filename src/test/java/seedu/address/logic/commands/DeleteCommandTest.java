35bc7c476392f363fb8e816f377ec40b581eab45
yalasoo <194611248+yalasoo@users.noreply.github.com>
2025-10-09 12:52:01 +0800

    Update DeleteCommandTest

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -105,7 +105,8 @@ public class DeleteCommandTest {
     public void toStringMethod() {
         Index targetIndex = Index.fromOneBased(1);
         DeleteCommand deleteCommand = new DeleteCommand(targetIndex);
-        String expected = DeleteCommand.class.getCanonicalName() + "{targetIndex=" + targetIndex + "}";
+        String expected = DeleteCommand.class.getCanonicalName() +
+                "{targetIndex=" + targetIndex + ", targetName=null, isDeletedByName=false}";
         assertEquals(expected, deleteCommand.toString());
     }
 
