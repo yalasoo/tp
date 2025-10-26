0389e9ff073da05744fc4a802f2dfbc135eb2353
deepa-m1 <dhulasidharandeepamalika@gmail.com>
2025-10-26 22:04:42 +0800

    Update ListCommandTest to pass test cases

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -1,16 +1,19 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Comparator;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
@@ -20,20 +23,28 @@ public class ListCommandTest {
     private Model model;
     private Model expectedModel;
 
-    @BeforeEach
-    public void setUp() {
+    @Test
+    public void execute_noFavourites_showsSameList() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
-    }
-
-    @Test
-    public void execute_listIsNotFiltered_showsSameList() {
         assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
-    public void execute_listIsFiltered_showsEverything() {
-        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+    public void execute_listHasFavourites_showsFavouritesOnTop() {
+        //showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+
+        Person thirdPerson = model.getFilteredPersonList().get(INDEX_THIRD_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(thirdPerson).withFavourite(true).build();
+        // So we mark third person as favourite in both actual model and expectedModel
+        model.setPerson(thirdPerson, editedPerson);
+        expectedModel.setPerson(thirdPerson, editedPerson);
+
+        // For expected model, thirdPerson should now move to top
+        expectedModel.sortFilteredPersonList(Comparator.comparing(Person::getIsFavBoolean).reversed());
+
         assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
     }
 }
