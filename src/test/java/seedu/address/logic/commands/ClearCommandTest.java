9319e4f682c80327e7676cc2ea8ee4c51e9e8c7d
yalasoo <194611248+yalasoo@users.noreply.github.com>
2025-11-03 21:45:45 +0800

    Update method signature

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -9,6 +9,7 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.ui.TestInfoInfoPopupHandler;
 
 public class ClearCommandTest {
 
@@ -16,8 +17,8 @@ public class ClearCommandTest {
     public void execute_emptyAddressBook_success() {
         Model model = new ModelManager();
         Model expectedModel = new ModelManager();
-
-        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        TestInfoInfoPopupHandler handler = new TestInfoInfoPopupHandler();
+        assertCommandSuccess(new ClearCommand(handler), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
@@ -25,8 +26,8 @@ public class ClearCommandTest {
         Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel.setAddressBook(new AddressBook());
-
-        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        TestInfoInfoPopupHandler handler = new TestInfoInfoPopupHandler();
+        assertCommandSuccess(new ClearCommand(handler), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
 }
--- src/test/java/seedu/address/ui/TestInfoInfoPopupHandler.java
+++ src/test/java/seedu/address/ui/TestInfoInfoPopupHandler.java
@@ -1,12 +1,14 @@
 package seedu.address.ui;
 
+import seedu.address.logic.commands.exceptions.CommandException;
+
 /**
- * A class handles showing delete error popup windows for test cases.
+ * A class handles showing message popup windows for test cases.
  */
 public class TestInfoInfoPopupHandler implements InfoPopupHandler {
 
     @Override
-    public void showMessage(String message) {
+    public void showMessage(String message, String instruction) throws CommandException {
         System.out.println(message);
     }
 }
